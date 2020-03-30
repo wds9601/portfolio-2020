cbb263037b70ea76449e8b30048b73ab1af1bd66
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-03-30 13:29:04 -0700

    nav link animation added

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,22 @@
 const navSlide = () => {
     const burger = document.querySelector('.nav-burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
     burger.addEventListener('click', () => {
-        console.log("Click")
+        //Toggle Nav menu
         nav.classList.toggle('nav-active')
+
+        //Animate Nav Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ``
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease backwards ${index / 7 + .5}s`
+            }
+        })
+
+        //Toggle 
     })
 }
 
